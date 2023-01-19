999a65d66897c1786a220c2e89cfa05882ca2c0c
Morning <morning@mstars-lab3-07s-iMac.local>
2023-01-19 12:42:38 +0800

    Lastupdate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Button from "./components/Button";
 import { Routes, Route } from "react-router-dom";
-import { Home } from './components/Home';
-import Header from './components/Header';
-import Main1 from "./components/Main1"
-import Main2 from './components/Main2';
-
+import { Home } from "./components/Home";
+import Header from "./components/Header";
+import Main1 from "./components/Main1";
+import Main2 from "./components/Main2";
+import Admin from "./admin/Admin";
 
 const imgList = {
   img1: require("./images /logo.png"),
   img2: require("./images /mmm.png"),
   img3: require("./images /Group.png"),
-}
+};
 const itemList = [
   { id: 1, title: "Our Story" },
   { id: 2, title: "Membership" },
@@ -21,27 +21,18 @@ const itemList = [
   { id: 4, title: "Sign In" },
 ];
 
-
-
 function App() {
-
   return (
     <div className="container-fluid p-0">
-
       <Navbar logo={imgList.img1} tses={itemList} />
-      
 
-      
       <Routes>
-        <Route  exact path='/' element={<Home/>} />
-        <Route path='/header' element={<Header />} />
-        <Route path='/main1' element={<Main1 />} />
-        <Route path='/main2' element={<Main2 />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/main1" element={<Main1 />} />
+        <Route path="/main2" element={<Main2 />} />
         <Route />
       </Routes>
-
-        
-
     </div>
   );
 }
